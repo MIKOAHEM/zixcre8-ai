91ca9bef787eb1aa277546f4bf193d66a40f46a1
MIKOAHEM <tfab1173@gmail.com>
2025-12-06 02:42:06 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    esmExternals: false,
-  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
-        fs: false,
+        fs: false
       };
     }
+
+    // Performance boosts for three.js & r3f
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      layers: true
+    };
+
+    config.ignoreWarnings = [
+      { module: /node_modules\/three/ }
+    ];
+
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+      three: require.resolve("three")
+    };
+
     return config;
-  },
+  }
 };
 
 module.exports = nextConfig;
