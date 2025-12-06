eea281555b8a18119fe36b1a2b7064434da22f35
MIKOAHEM <tfab1173@gmail.com>
2025-12-06 02:55:42 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Zixcre8 AI</h1>
+      <p>Your Next.js app is running successfully.</p>
+    </div>
+  );
+}
