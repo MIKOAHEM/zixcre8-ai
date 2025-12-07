f6b09133981025747f7325a292231970ba963c83
MIKOAHEM <tfab1173@gmail.com>
2025-12-06 19:13:09 -0500

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,1 +1,24 @@
+"use client";
 
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+export default function Scene() {
+  return (
+    <div style={{ width: "100%", height: "400px" }}>
+      <Canvas shadows camera={{ position: [3, 2, 3], fov: 40 }}>
+        <color attach="background" args={["#000"]} />
+
+        <mesh castShadow>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="#00ffea" />
+        </mesh>
+
+        <ambientLight intensity={0.4} />
+        <directionalLight position={[5, 5, 5]} intensity={1} castShadow />
+
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
+}
