1cf3ca205d041b433bc794d5cecebfb9d2eb8b4c
MIKOAHEM <tfab1173@gmail.com>
2025-12-08 01:38:05 -0500

    Update Scene.jsx

--- components/Scene.jsx
+++ components/Scene.jsx
@@ -1,38 +1,20 @@
 "use client";
 
-import { Suspense, useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, Loader, Environment, Html } from "@react-three/drei";
-import { MeshStandardMaterial } from "three";
-
-function RotatingModel() {
-  const ref = useRef();
-  useFrame((state, delta) => {
-    ref.current.rotation.y += delta * 0.4;
-    ref.current.rotation.x = Math.sin(state.clock.elapsedTime * 0.2) * 0.05;
-  });
-
-  return (
-    <mesh ref={ref} position={[0, -0.2, 0]} scale={[1.6,1.6,1.6]} castShadow>
-      <icosahedronGeometry args={[0.9, 2]} />
-      <meshStandardMaterial roughness={0.2} metalness={0.9} color={"#00ffd5"} />
-    </mesh>
-  );
-}
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 export default function Scene() {
   return (
-    <>
-      <Canvas camera={{ position: [2.7, 1.2, 3.5], fov: 45 }} shadows>
-        <ambientLight intensity={0.35} />
-        <directionalLight intensity={0.9} position={[5, 6, 2]} castShadow />
-        <Suspense fallback={<Html center>Loading 3D…</Html>}>
-          <RotatingModel />
-          <Environment preset="studio" />
-        </Suspense>
-        <OrbitControls enablePan={false} autoRotate autoRotateSpeed={0.7} />
+    <div style={{ height: "500px", width: "100%" }}>
+      <Canvas>
+        <ambientLight intensity={1.2} />
+        <pointLight position={[10, 10, 10]} />
+        <mesh>
+          <sphereGeometry args={[1, 32, 32]} />
+          <meshStandardMaterial color="#00f7ff" roughness={0.1} metalness={1} />
+        </mesh>
+        <OrbitControls />
       </Canvas>
-      <Loader />
-    </>
+    </div>
   );
 }
