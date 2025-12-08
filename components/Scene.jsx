57c6e71d646b15bec32dec7437fc365619306e14
MIKOAHEM <tfab1173@gmail.com>
2025-12-08 01:47:14 -0500

    Update Scene.jsx

--- components/Scene.jsx
+++ components/Scene.jsx
@@ -1,19 +1,23 @@
 "use client";
-
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Sphere } from "@react-three/drei";
 
 export default function Scene() {
   return (
-    <div style={{ height: "500px", width: "100%" }}>
-      <Canvas>
-        <ambientLight intensity={1.2} />
-        <pointLight position={[10, 10, 10]} />
-        <mesh>
-          <sphereGeometry args={[1, 32, 32]} />
-          <meshStandardMaterial color="#00f7ff" roughness={0.1} metalness={1} />
-        </mesh>
-        <OrbitControls />
+    <div style={{ height: "400px", marginTop: "50px" }}>
+      <Canvas camera={{ position: [0, 0, 4] }}>
+        <ambientLight intensity={0.3} />
+        <directionalLight position={[2, 2, 2]} intensity={1} />
+
+        <Sphere args={[1, 64, 64]}>
+          <meshStandardMaterial
+            color="#00eaff"
+            metalness={0.9}
+            roughness={0.1}
+          />
+        </Sphere>
+
+        <OrbitControls enableZoom={false} />
       </Canvas>
     </div>
   );
