2cd90ec45cc69fea3f4aaaf8d4bf77012a5974f2
MIKOAHEM <tfab1173@gmail.com>
2025-12-10 19:25:07 -0500

    Delete app/page.js

--- app/page.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Welcome to ZixCre8 AI</h1>
-      <p>Your creative AI studio is running successfully.</p>
-    </div>
-  );
-}
