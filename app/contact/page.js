abb8dc3fb19a29e8071fb154044cb7ec878083b6
MIKOAHEM <tfab1173@gmail.com>
2025-12-13 00:18:47 -0500

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,12 +1,12 @@
 export default function ContactPage() {
   return (
-    <div className="page-container">
+    <section className="page-container">
       <h1>Contact Us</h1>
 
-      <p>Email: zixcre8@gmail.com</p>
-      <p>Phone: +2347017512874</p>
-      <p>WhatsApp: <a href="https://wa.me/2347017512874">Chat on WhatsApp</a></p>
-      <p>TikTok: @mikoahem</p>
-    </div>
+      <p>Email: <strong>zixcre8@gmail.com</strong></p>
+      <p>Phone: <strong>+2347017512874</strong></p>
+      <p>WhatsApp: <a href="https://wa.me/2347017512874">Open Chat</a></p>
+      <p>TikTok: <strong>@mikoahem</strong></p>
+    </section>
   );
 }
