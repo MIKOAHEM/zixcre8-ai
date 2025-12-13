e88543e8c11fc7553c4beef855e40a6d7424e8e0
MIKOAHEM <tfab1173@gmail.com>
2025-12-13 14:39:12 -0500

    Update page.js

--- app/services/page.js
+++ app/services/page.js
@@ -1,8 +1,3 @@
-export default function ServicesPage() {
-  return (
-    <div className="page-container">
-      <h1>Our Services</h1>
-      <p>We provide AI-powered digital solutions.</p>
-    </div>
-  );
+export default function Services() {
+  return <h1>Our Services</h1>;
 }
