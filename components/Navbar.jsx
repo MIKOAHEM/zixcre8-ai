c7197811affa1fd0ab78620b0cfa69c1ec124a1e
MIKOAHEM <tfab1173@gmail.com>
2025-12-13 14:36:37 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,17 @@
 "use client";
-
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="logo">ZixCre8</div>
+    <header className="nav">
+      <div className="brand">ZixCre8 AI</div>
 
-      <ul className="nav-links">
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/services">Tools</Link></li>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
-      </ul>
-
-      <a className="btn small" href="/contact">Get Started</a>
-    </nav>
+      <nav className="nav-links">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/services">Services</Link>
+        <Link href="/contact">Contact</Link>
+      </nav>
+    </header>
   );
 }
